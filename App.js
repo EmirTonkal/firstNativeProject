52dbe90f7f322777cff74c101a8717e7e365f9b6
EmirTonkal <emirtonkal97@gmail.com>
2022-02-28 14:23:29 +0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text style={styles.deneme}>emir</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +17,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  deneme: {
+    justifyContent:'center',
+    flexDirection:'row',
   },
 });
